12cf69471f5efef9348344c2b580480d940b2d5c
mdridoyhasan <mdridoyhasan215246@gmail.com>
2024-08-18 16:02:53 +0600

    tea house site upload done for desktop

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  plugins: [
+    plugin(function({ addUtilities, addComponents, e, config }) {
+      // Add your custom styles here
+    }),
+  ]
+}
